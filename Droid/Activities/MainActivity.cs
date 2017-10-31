9aff90860935a543652e0fcfcc2588a30315ef4f
Jacob Van Brunt <jvanbrunt@developertown.com>
2017-10-31 09:58:35 -0400

    Creating clean repo.

---
+++ Droid/Activities/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace devfest_2017.Droid
+{
+    [Activity(Label = "@string/app_name", Icon = "@mipmap/icon",
+        LaunchMode = LaunchMode.SingleInstance,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        ScreenOrientation = ScreenOrientation.Portrait)]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+    }
+
+}
